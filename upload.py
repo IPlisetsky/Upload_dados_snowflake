1f746dd19b9cd9d1ab2057b6d0f2ef68134315e5
Igor Gabriel <81106970+IPlisetsky@users.noreply.github.com>
2025-03-14 08:48:48 -0300

    Update upload.py

--- upload.py
+++ upload.py
@@ -8,7 +8,6 @@ import pandas as pd
 def snow_flake_api():  
     print("ABRIU")
  
-    # Conexão ao Snowflake
     snowflake_config = {
     'user': '',
     'password': '',
@@ -22,17 +21,13 @@ def snow_flake_api():
     con = connect(**snowflake_config)
     cursor = con.cursor()
  
-    # Carregar a planilha
-    CaminhoArq_ = #CAMINHO DO ARQUIVO
+    CaminhoArq_ =
     wb = load_workbook(filename=CaminhoArq_, data_only=True)
     sheet = wb.active
-
-    # Prepara dados para inserir
     dados = []
-    for row in sheet.iter_rows(2, sheet.max_row):  # começando da segunda linha para pular o cabeçalho para inputar os dados
-        dados.append([str(cell.value).replace(',', '') if cell.value is not None else 'NULL' for cell in row])  # converte todos os valores para string, remove vírgulas
+    for row in sheet.iter_rows(2, sheet.max_row): 
+        dados.append([str(cell.value).replace(',', '') if cell.value is not None else 'NULL' for cell in row]) 
 
-    # Inserir dados em lote
     total_rows = len(dados)
     batch_size = 10000  
     total_inserted = 0
@@ -40,12 +35,11 @@ def snow_flake_api():
     for i in range(0, total_rows, batch_size):
         batch = dados[i:i + batch_size]
         placeholders = ', '.join(['%s'] * len(batch[0]))
-        query = f"INSERT INTO VALUES ({placeholders})" # Executa a query de carga
+        query = f"INSERT INTO VALUES ({placeholders})"
         cursor.executemany(query, batch)
         total_inserted += len(batch)
         print(f'Inseridas {total_inserted} de {total_rows} linhas')
 
-    # Fecha conexão
     cursor.close()
     con.close()
  
